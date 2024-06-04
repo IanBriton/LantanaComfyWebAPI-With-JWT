041c7ab75d38cd00ca5902b737c3cf48c74241cb
IanBriton <danakutuya@gmail.com>
2024-06-04 15:28:18 +0300

    Add Dto and static roles

---
+++ LantanaComfyAPI/Dto/UpdatePermissionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LantanaComfyAPI.Dto
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+    }
+}
